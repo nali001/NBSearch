97d9d66797278448df8ecb002cf24a37f181ca60
mns-gpu <n.li@uva.nl>
2021-10-20 16:01:14 +0000

    Get frontend to run

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 # mian.py is just a program entry and nothing else. 
 # It creates a PWD that is under the root directory of the project, i.e., NbSearch/
-from nbtest import predict_test
-predict_test()
+from nbtest import doc2vec_test
+doc2vec_test()
